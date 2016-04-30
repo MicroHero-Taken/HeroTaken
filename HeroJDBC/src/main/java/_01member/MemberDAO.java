a9a27396d3ea183a4ab3cd4dc8c30d5373ce7142
Tim <fudon1216@yahoo.com.tw>
2016-04-30 10:58:55 +0800

    member

---
+++ HeroJDBC/src/main/java/_01member/MemberDAO.java
@@ -0,0 +1,28 @@
+package _01member;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface MemberDAO {
+
+	MemberBean select(int id);
+
+	List<MemberBean> getAllMembers();
+
+	List<MemberBean> select();
+
+	MemberBean insertMember(MemberBean bean) throws SQLException;
+
+	MemberBean update(int memberNo, String email, String password, String memberName, String family_name,
+			String given_name, String gender, String id, String phone, int rating, int experience, int coin,
+			java.sql.Timestamp birthday, int memberStatusNo, int memberHeroStatus);
+
+	int delete(int memberNo);
+
+	MemberBean select(String email);
+	
+	MemberBean selectEmail(String email);
+
+	MemberBean insertGoogle(MemberBean bean);
+
+}
